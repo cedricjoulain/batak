c121ef19ff520fff29a21a8539e75ce071fd1fa6
Cedric Joulain <cedric.joulain@gmail.com>
2024-12-24 16:08:58 +0100

    add: handles all leds and buttons

--- batak.py
+++ batak.py
@@ -3,15 +3,25 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(26, GPIO.IN)
+buttons = [6, 13, 19, 26]
+leds = [12, 16, 20, 21]
+
+
+for i, button in enumerate(buttons):
+    GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+for i, led in enumerate(leds):
+    GPIO.setup(led, GPIO.OUT)
+    GPIO.output(led, 0)
 
 try:
     while True:
-        if GPIO.input(26):
-            GPIO.output(21, 0)
-        else:
-            GPIO.output(21, 1)
-        time.sleep(0.001)
+        for i, button in enumerate(buttons):
+            if GPIO.input(button):
+                GPIO.output(leds[i], 0)
+            else:
+                GPIO.output(leds[i], 1)
+        time.sleep(0.05)
 except KeyboardInterrupt:
-    GPIO.cleanup
+    print("Stop and cleanup")
+    GPIO.cleanup()
