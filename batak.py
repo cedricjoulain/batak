54ae2a16ba24c44a1e54cf5eae0d4a116362470a
Cedric Joulain <cedric.joulain@gmail.com>
2024-12-24 16:52:54 +0100

    change: nicer const

--- batak.py
+++ batak.py
@@ -4,30 +4,29 @@ import time
 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+import const
 
-BUTTONS = [6, 13, 19, 26]
-LEDS = [12, 16, 20, 21]
+GPIO.setmode(GPIO.BCM)
 
 def test():
     """Light led if button on"""
     while True:
-        for i, button in enumerate(BUTTONS):
+        for i, button in enumerate(const.BUTTONS):
             if GPIO.input(button):
-                GPIO.output(LEDS[i], 0)
+                GPIO.output(const.LEDS[i], 0)
             else:
-                GPIO.output(LEDS[i], 1)
+                GPIO.output(const.LEDS[i], 1)
         time.sleep(0.05)
 
 def game():
     """One 60 seconds game"""
-    target = random.randint(0, len(LEDS))
+    target = random.randint(0, len(const.LEDS))
     score = 0
-    GPIO.output(LEDS[target], 1)
+    GPIO.output(const.LEDS[target], 1)
     start = time.time()
     while time.time() - start < 60:
         found = -1
-        for i, button in enumerate(BUTTONS):
+        for i, button in enumerate(const.BUTTONS):
             if not GPIO.input(button):
                 # button down is 0
                 if found == -1:
@@ -37,27 +36,28 @@ def game():
         if found == target:
             #GOOD!
             score += 1
-            GPIO.output(LEDS[target], 0)
-            newtarget = random.randint(0, len(LEDS))
+            GPIO.output(const.LEDS[target], 0)
+            newtarget = random.randint(0, len(const.LEDS))
             while newtarget == target:
-                newtarget = random.randint(0, len(LEDS))
+                newtarget = random.randint(0, len(const.LEDS))
             target = newtarget
-            GPIO.output(LEDS[target], 1)
+            GPIO.output(const.LEDS[target], 1)
         else:
             time.sleep(0.05)
-    GPIO.output(LEDS[target], 0)
+    GPIO.output(const.LEDS[target], 0)
     return score
 
 def main():
     """Batak game, maximum light switch off in 60 seconds"""
-    for button in BUTTONS:
+    for button in const.BUTTONS:
         GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    for led in LEDS:
+    for led in const.LEDS:
         GPIO.setup(led, GPIO.OUT)
         GPIO.output(led, 0)
 
     try:
+        print("Start game in 10 seconds")
         time.sleep(10)
         print("Game started")
         score = game()
